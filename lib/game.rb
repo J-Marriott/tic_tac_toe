e824e414ecbd02a48e85f0d6164bb29b6a27430d
Joe Marriott <marriott90@gmail.com>
2017-03-10 13:45:15 +0000

    player1 or player2 selected randomly to start game, switches after turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  WINNING_COMBOS = [
+  WIN = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -11,10 +11,33 @@ class Game
     [3, 5, 7]
   ].freeze
 
-  attr_reader :gameboard
-  
-  def initialize
-    @gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  PLAYERS = [1, 2].freeze
+
+  attr_reader :board, :turn, :player1, :player2
+
+  def initialize(startplayer: PLAYERS.sample)
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @turn = startplayer
+    @player1 = []
+    @player2 = []
+  end
+
+  def picksquare(n)
+    if @board.include? n
+      @board.delete(n)
+      add_to_player(n)
+      player_switch
+    else
+      'square taken, pick again'
+    end
+  end
+
+  def add_to_player(n)
+    @turn == 1 ? @player1 << n : @player2 << n
+  end
+
+  def player_switch
+    @turn == 1 ? @turn = 2 : @turn = 1
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,38 @@ require 'game'
 describe Game do
 
   let(:game){described_class.new}
-  
-  it "initializes with a new gameboard" do
-    expect(game.gameboard).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  describe '#initialize' do
+    it 'initializes with a new gameboard' do
+      expect(game.board).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end
+    it 'initializes with 2 players' do
+      expect(game.player1).to eq []
+      expect(game.player2).to eq []
+    end
+    it 'the first turn can be player1 or player2' do
+      expect([1, 2]).to include(game.turn)
+
+    end
   end
+
+  describe '#picksquare' do
+    it 'deletes the value from board array if unselected' do
+      game.picksquare(5)
+      expect(game.board).to eq [1, 2, 3, 4, 6, 7, 8, 9]
+    end
+    it 'gives message if the value from board has already been selected' do
+      game.picksquare(5)
+      expect(game.picksquare(5)).to eq 'square taken, pick again'
+    end
+  end
+
+  describe '#add_to_player' do
+    it 'allocates the square to the player whose turn it is' do
+      game.instance_variable_set(:@turn, 1)
+      game.picksquare(5)
+      expect(game.player1).to eq [5]
+    end
+  end
+
 end
